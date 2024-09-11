f868850f3bb503fabc98c25b066cf3bc4db81fc9
rudrakumar2012 <manaw10071994@gmail.com>
2024-09-11 20:58:32 +0530

    variable

--- 01Intro/variableMe.js
+++ 01Intro/variableMe.js
@@ -3,3 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello John";
 greetings.toUpperCase();
 console.log(greetings);
+//number
+//let userId: number = 334455;
+var userId = 334;
+userId.toFixed();
+//boolean
+var isLoggedIn = false;
--- 01Intro/variableMe.ts
+++ 01Intro/variableMe.ts
@@ -1,7 +1,18 @@
 let greetings: string = "Hello John";
 
 greetings.toUpperCase();
-
 console.log(greetings);
+
+//number
+
+//let userId: number = 334455;
+let userId = 334;
+userId.toFixed();
+
+//boolean
+let isLoggedIn: boolean = false;
+
+//any
+
 
 export {}
